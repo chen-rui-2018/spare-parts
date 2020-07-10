4c1beb8e055a9e088d1ae76b5f8ff4d731c76d7d
chenr <chenr@op-mobile.com.cn>
2020-07-10 19:45:09 +0800

    查询模板修改

--- src/main/java/com/xj/work/spare/service/impl/SparePortsServiceImpl.java
+++ src/main/java/com/xj/work/spare/service/impl/SparePortsServiceImpl.java
@@ -1,10 +1,17 @@
 package com.xj.work.spare.service.impl;
 
+import com.xj.work.common.Filters;
 import com.xj.work.common.PaginationData;
+import com.xj.work.common.Params;
+import com.xj.work.spare.model.SparePartsQuery;
 import com.xj.work.spare.service.SparePortsService;
 import com.xj.work.spare.vo.SparePartsVo;
+import org.springframework.stereotype.Service;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * @author： chenr
@@ -12,24 +19,72 @@ import java.util.List;
  * @version： v1.0
  * @modified By:
  */
+@Service
 public class SparePortsServiceImpl implements SparePortsService {
     @Override
     public String addSpareParts(SparePartsVo sparePartsVo) {
-        return null;
+        try {
+//            String result =   new  HttpUtil().postForObject("/AssetCenter/spareParts/info/add",sparePartsVo,String.class);
+            String result = UUID.randomUUID().toString().replaceAll("-","");
+            return result;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+       return null;
     }
 
     @Override
     public String updateSpareParts(SparePartsVo sparePartsVo) {
+        try {
+//            String result =   new  HttpUtil().postForObject("/AssetCenter/spareParts/info/update",sparePartsVo,String.class);
+            String result = UUID.randomUUID().toString().replaceAll("-","");
+            return result;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
         return null;
+
     }
 
     @Override
     public String deleteSpareParts(SparePartsVo sparePartsVo) {
-        return null;
+        try {
+    //      String result =   new  HttpUtil().postForObject("/AssetCenter/spareParts/info/delete",sparePartsVo,String.class);
+            String result = UUID.randomUUID().toString().replaceAll("-","");
+            return result;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+            return null;
     }
 
     @Override
-    public PaginationData<List<SparePartsVo>> getSparePartsList(SparePartsVo sparePartsVo) {
+    public PaginationData<List<SparePartsVo>> getSparePartsList(SparePartsQuery query){
+        Params params = getParams(query,new SparePartsVo());
         return null;
     }
+
+    private Params getParams(SparePartsQuery query,SparePartsVo vo) {
+        Params params = new Params();
+        List<Filters> filtersList = new ArrayList<>();
+        if(query.getCritical()!=null) {
+            Filters filters = new Filters();
+            filters.setFieldName("critical");
+            filters.setCompare("=");
+            filters.setFieldValue(query.getCritical());
+            filtersList.add(filters);
+        }
+        Class spare = vo.getClass();
+        Field[] fields = spare.getFields();
+        StringBuilder  stringBuilder = new StringBuilder();
+        for (Field field : fields){
+            stringBuilder.append(field.getName()+",");
+        }
+        params.setFields( stringBuilder.toString().substring(0,stringBuilder.length()-1));
+        params.setFilters(filtersList);
+        params.setPage(query.getPage()==0?1:query.getPage());
+        params.setPerPage(query.getRows()==0?20:query.getRows());
+        return params;
+    }
+
 }
--- src/main/java/com/xj/work/spare/vo/SparePartsVo.java
+++ src/main/java/com/xj/work/spare/vo/SparePartsVo.java
@@ -1,13 +1,10 @@
 package com.xj.work.spare.vo;
 
-import com.xj.work.spare.model.AssetNature;
-import com.xj.work.spare.model.ProfessionalKind;
-import com.xj.work.spare.model.SparePartsStatus;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.sql.Blob;
 import java.util.Date;
 
 /**
@@ -44,7 +41,7 @@ public class SparePartsVo {
     @ApiModelProperty(value = "出厂日期")
     private Date manufactureDate;
     @ApiModelProperty(value = "资产性质")
-    private AssetNature assetNature;
+    private String assetNature;
     @ApiModelProperty(value = "资产单位")
     private String assetOwner;
     @ApiModelProperty(value = "资产编号")
@@ -56,27 +53,27 @@ public class SparePartsVo {
     @ApiModelProperty(value = "货架位置")
     private String sparePartsStoreLoc;
     @ApiModelProperty(value = "专业")
-    private ProfessionalKind major;
+    private String major;
     @ApiModelProperty(value = "备品备件状态")
-    private SparePartsStatus sparePartsStatus;
+    private String sparePartsStatus;
     @ApiModelProperty(value = "是否完整")
     private Boolean complete;
-    @ApiModelProperty(value = "是否打印")
+    @ApiModelProperty(value = "是否打印",example = "true")
     private Boolean print;
-    @ApiModelProperty(value = "铭牌图片/照片信息")
-    private Blob nameplatePictureInf;
-    @ApiModelProperty(value = "正面图信息")
-    private Blob frontInf;
-    @ApiModelProperty(value = "背面图信息")
-    private Blob backInf;
-    @ApiModelProperty(value = "左侧图信息")
-    private Blob leftInf;
-    @ApiModelProperty(value = "右侧图信息")
-    private Blob rightInf;
-    @ApiModelProperty(value = "备品二维码")
+    @ApiModelProperty(value = "铭牌图片/照片信息",example = "二维码图信息")
+    private MultipartFile nameplatePictureInf;
+    @ApiModelProperty(value = "正面图信息",example = "二维码图信息")
+    private MultipartFile frontInf;
+    @ApiModelProperty(value = "背面图信息",example = "二维码图信息")
+    private MultipartFile backInf;
+    @ApiModelProperty(value = "左侧图信息",example = "二维码图信息")
+    private MultipartFile leftInf;
+    @ApiModelProperty(value = "右侧图信息",example = "二维码图信息")
+    private MultipartFile rightInf;
+    @ApiModelProperty(value = "备品二维码",example = "二维码图信息")
     private String qRcode;
-    @ApiModelProperty(value = "二维码图信息")
-    private Blob qRcodeInf;
+    @ApiModelProperty(value = "二维码图信息",example = "二维码图信息")
+    private MultipartFile qRcodeInf;
 
 
 
