ed4c2d738d227733c6fae6bb910dfefbf97302d2
chenr <chenr@op-mobile.com.cn>
2020-07-11 14:33:54 +0800

    备品备件台账查询接口

--- src/main/java/com/xj/work/spare/service/impl/SparePortsServiceImpl.java
+++ src/main/java/com/xj/work/spare/service/impl/SparePortsServiceImpl.java
@@ -75,16 +75,135 @@ public class SparePortsServiceImpl implements SparePortsService {
         return null;
     }
 
+    /**
+     * 组装统一入参
+     * @param query
+     * @param vo
+     * @return
+     */
     private Params getParams(SparePartsQuery query,SparePartsVo vo) {
         Params params = new Params();
         List<Filters> filtersList = new ArrayList<>();
-        if(query.getCritical()!=null) {
+        if(query.getSparePartsType()!= null) {
             Filters filters = new Filters();
-            filters.setFieldName("critical");
+            filters.setFieldName("sparePartsType");
             filters.setCompare("=");
-            filters.setFieldValue(query.getCritical());
+            filters.setFieldValue(query.getSparePartsType());
             filtersList.add(filters);
         }
+        if(query.getName()!= null) {
+            Filters filters = new Filters();
+            filters.setFieldName("name");
+            filters.setCompare("=");
+            filters.setFieldValue(query.getName());
+            filtersList.add(filters);
+        }
+        if(query.getProvince()!= null) {
+            Filters filters = new Filters();
+            filters.setFieldName("province");
+            filters.setCompare("=");
+            filters.setFieldValue(query.getProvince());
+            filtersList.add(filters);
+        }
+        if(query.getStation()!= null) {
+            Filters filters = new Filters();
+            filters.setFieldName("station");
+            filters.setCompare("=");
+            filters.setFieldValue(query.getStation());
+            filtersList.add(filters);
+        }
+        if(query.getProductAssetModel()!= null) {
+            Filters filters = new Filters();
+            filters.setFieldName("productAssetModel");
+            filters.setCompare("=");
+            filters.setFieldValue(query.getProductAssetModel());
+            filtersList.add(filters);
+        }
+        if(query.getManufactureCountry()!= null) {
+            Filters filters = new Filters();
+            filters.setFieldName("manufactureCountry");
+            filters.setCompare("=");
+            filters.setFieldValue(query.getManufactureCountry());
+            filtersList.add(filters);
+        }
+        if(query.getManufacturer()!= null) {
+            Filters filters = new Filters();
+            filters.setFieldName("manufacturer");
+            filters.setCompare("=");
+            filters.setFieldValue(query.getManufacturer());
+            filtersList.add(filters);
+        }
+        if(query.getMaintainOrg()!= null) {
+            Filters filters = new Filters();
+            filters.setFieldName("maintainOrg");
+            filters.setCompare("=");
+            filters.setFieldValue(query.getMaintainOrg());
+            filtersList.add(filters);
+        }
+        if(query.getMaintainGroup()!= null) {
+            Filters filters = new Filters();
+            filters.setFieldName("maintainGroup");
+            filters.setCompare("=");
+            filters.setFieldValue(query.getMaintainGroup());
+            filtersList.add(filters);
+        }
+        if(query.getManufactureDate()!= null) {
+            Filters filters = new Filters();
+            filters.setFieldName("manufactureDate");
+            filters.setCompare("=");
+            filters.setFieldValue(query.getManufactureDate());
+            filtersList.add(filters);
+        }
+        if(query.getAssetNature()!= null) {
+            Filters filters = new Filters();
+            filters.setFieldName("assetNature");
+            filters.setCompare("=");
+            filters.setFieldValue(query.getAssetNature());
+            filtersList.add(filters);
+        }
+        if(query.getAssetOwner()!= null) {
+            Filters filters = new Filters();
+            filters.setFieldName("assetOwner");
+            filters.setCompare("=");
+            filters.setFieldValue(query.getAssetOwner());
+            filtersList.add(filters);
+        }
+        if(query.getSerialNumber()!= null) {
+            Filters filters = new Filters();
+            filters.setFieldName("serialNumber");
+            filters.setCompare("=");
+            filters.setFieldValue(query.getSerialNumber());
+            filtersList.add(filters);
+        }
+        if(query.getRatedVoltage()!= null) {
+            Filters filters = new Filters();
+            filters.setFieldName("ratedVoltage");
+            filters.setCompare("=");
+            filters.setFieldValue(query.getRatedVoltage());
+            filtersList.add(filters);
+        }
+        if(query.getSparePartsWareHouse()!= null) {
+            Filters filters = new Filters();
+            filters.setFieldName("sparePartsWareHouse");
+            filters.setCompare("=");
+            filters.setFieldValue(query.getSparePartsWareHouse());
+            filtersList.add(filters);
+        }
+        if(query.getMajor()!= null) {
+            Filters filters = new Filters();
+            filters.setFieldName("major");
+            filters.setCompare("=");
+            filters.setFieldValue(query.getMajor());
+            filtersList.add(filters);
+        }
+        if(query.getComplete()!= null) {
+            Filters filters = new Filters();
+            filters.setFieldName("complete");
+            filters.setCompare("=");
+            filters.setFieldValue(query.getComplete());
+            filtersList.add(filters);
+        }
+
         Class spare = vo.getClass();
         Field[] fields = spare.getDeclaredFields();
         StringBuilder  stringBuilder = new StringBuilder();
