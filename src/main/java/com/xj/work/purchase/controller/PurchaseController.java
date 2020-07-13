967a64bfde297cb54b5e6c055b3674303fb9c6d5
jianglinfeng <736036375@qq.com>
2020-07-13 16:03:03 +0800

    采购

--- src/main/java/com/xj/work/purchase/controller/PurchaseController.java
+++ src/main/java/com/xj/work/purchase/controller/PurchaseController.java
@@ -1,9 +1,16 @@
 package com.xj.work.purchase.controller;
 
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @description
  * @Author jianglinfeng
  * @Date 2020/7/11 0011
  **/
+@Api(tags = "采购")
+@RestController
+@RequestMapping("/work/quota")
 public class PurchaseController {
 }
